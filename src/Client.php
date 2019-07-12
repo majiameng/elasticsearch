a71af6b00814ac4758e8811d3d6760828db8de6e
majiameng <666@majiameng.com>
2019-07-12 18:22:30 +0800

    first commit

---
+++ src/Client.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Elasticsearch
+ * @author: JiaMeng <666@majiameng.com>
+ */
+
+namespace tinymeng\Elasticsearch;
+
+abstract class Client
+{
+
+    /**
+     * Description:  init
+     * @author: JiaMeng <666@majiameng.com>
+     * Updater:
+     * @param $gateway
+     * @param null $config
+     * @return mixed
+     * @throws \Exception
+     */
+    protected static function init($gateway, $config = null)
+    {
+        $baseConfig = [
+            'app_id'    => '',
+            'app_secret'=> '',
+            'callback'  => '',
+            'scope'     => '',
+        ];
+        $gateway = Str::uFirst($gateway);
+        $class = __NAMESPACE__ . '\\Gateways\\' . $gateway;
+        if (class_exists($class)) {
+            $app = new $class(array_replace_recursive($baseConfig,$config));
+            if ($app instanceof GatewayInterface) {
+                return $app;
+            }
+            throw new \Exception("第三方登录基类 [$gateway] 必须继承抽象类 [GatewayInterface]");
+        }
+        throw new \Exception("第三方登录基类 [$gateway] 不存在");
+    }
+
+    /**
+     * Description:  __callStatic
+     * @author: JiaMeng <666@majiameng.com>
+     * Updater:
+     * @param $gateway
+     * @param $config
+     * @return mixed
+     */
+    public static function __callStatic($gateway, $config)
+    {
+        return self::init($gateway, ...$config);
+    }
+
+}
